7c21fa076e409e8781d4bfebb0369beb24a89533
krlspj <krls.pjds@gmail.com>
2020-10-06 01:59:38 +0200

    finish add meals to orders and live refresh orders

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+'use strict'
+
+let mealsState = []
 const stringToHTML = (s) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(s, 'text/html')
@@ -6,18 +9,74 @@ const stringToHTML = (s) => {
 
 const renderItem = (item) => {
     const element = stringToHTML(`<li data-id="${item._id}">${item.name}</li>`)
-    console.log(element)
+    element.addEventListener('click', () => {
+        const mealsList = document.getElementById('meals-list')
+        Array.from(mealsList.children).forEach(el => el.classList.remove('selected'))
+        element.classList.add('selected')
+        const mealsInput = document.getElementById('meals-id')
+        mealsInput.value = item._id
+    })
+    return element
+}
+
+const renderOrder = (order, meals) => {
+    const meal = meals.find(meal => meal._id === order.meal_id)
+    const element = stringToHTML(`<li data-id="${order._id}">${meal.name}\
+     - ${order.user_id}</li>`)
     return element
 }
 
 window.onload = () => {
-fetch('https://serverless-vercel.krlspj.vercel.app/api/meals')
-    .then(res => res.json())
-    .then(data => {
-        const mealsList = document.getElementById('meals-list')
-        const submit = document.getElementById('submit')
-        const listItems = data.map(renderItem)
-        listItems.forEach(elem => mealsList.appendChild(elem))
-        submit.removeAttribute('disabled')
+    const orderForm = document.getElementById('order')
+    orderForm.onsubmit = (e) => {
+        e.preventDefault()
+        const mealId = document.getElementById('meals-id')
+        const mealIdValue = mealId.value
+        if(!mealIdValue) {
+            alert('Debe seleccionar un plato')
+            return
+        }
+
+        const order = {
+            meal_id: mealIdValue,
+            user_id: 'chanchito feliz'
+        }
+        fetch('https://serverless-vercel.krlspj.vercel.app/api/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        }).then(x => x.json())
+          .then(res => {
+              const renderedOrder = renderOrder(res,mealsState)
+              const ordersList = document.getElementById('orders')
+              ordersList.appendChild(renderedOrder)
+          })
+
+    }
+
+    
+
+    
+    fetch('https://serverless-vercel.krlspj.vercel.app/api/meals')
+        .then(res => res.json())
+        .then(data => {
+            mealsState = data
+            const mealsList = document.getElementById('meals-list')
+            mealsList.innerHTML = ''
+            const submit = document.getElementById('submit')
+            const listItems = data.map(renderItem)
+            listItems.forEach(elem => mealsList.appendChild(elem))
+            submit.removeAttribute('disabled')
+            fetch('https://serverless-vercel.krlspj.vercel.app/api/orders')
+                .then(res => res.json())
+                .then(ordersData => {
+                    const ordersList = document.getElementById('orders')
+                    const listOrders = ordersData.map(order => renderOrder(order, data))
+                    ordersList.innerHTML = ''
+                    listOrders.forEach(order => ordersList.appendChild(order))
+                    console.log('orders -> ', ordersData)
+                })
     })
 }
